fd326db22fea6b6bac6280fee09eed42c8e043e0
okanbeydanol <60093192+okanbeydanol@users.noreply.github.com>
2025-08-13 20:10:32 +0200

    change js to promise base 1.0.2

--- www/CallNumber.js
+++ www/CallNumber.js
@@ -1,15 +1,21 @@
 function CallNumber() { }
 
-CallNumber.prototype.callNumber = function (success, error, number, bypassAppChooser) {
-    cordova.exec(success, error, 'CallNumber', 'callNumber', [number, bypassAppChooser]);
+// Promise-based callNumber
+CallNumber.prototype.callNumber = function (number, bypassAppChooser) {
+    return new Promise((resolve, reject) => {
+        cordova.exec(resolve, reject, 'CallNumber', 'callNumber', [number, bypassAppChooser]);
+    });
 };
 
-CallNumber.prototype.isCallSupported = function (success, error) {
-    cordova.exec(success, error, 'CallNumber', 'isCallSupported', []);
+// Promise-based isCallSupported
+CallNumber.prototype.isCallSupported = function () {
+    return new Promise((resolve, reject) => {
+        cordova.exec(resolve, reject, 'CallNumber', 'isCallSupported', []);
+    });
 };
 
 module.exports = new CallNumber();
-module.exports.CallNumber = module.exports; // Add this line
+module.exports.CallNumber = module.exports;
 
 // For ES module import support
 if (typeof window !== 'undefined' && window.cordova && window.cordova.plugins) {
